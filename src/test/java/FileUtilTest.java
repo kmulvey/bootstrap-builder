e895e7e17dbd277fa641979539f7133a07154c93
Kevin Mulvey <kmulvey@shutterstock.com>
2013-09-24 10:17:16 -0400

    actually test the whole thing

--- src/test/java/FileUtilTest.java
+++ src/test/java/FileUtilTest.java
@@ -32,6 +32,7 @@ public class FileUtilTest {
 	}
 	@Test
 	public void merge() {
+		String expected = ".well{min-height: 20px;padding: 19px;background-color: @well-bg;border-radius: @border-radius-base;.box-shadow(inset 0 1px 1px rgba(0,0,0,.05));blockquote{border-color: rgba(0,0,0,.15);border-color: rgba(255, 255, 255);&:focus{margin: 10px;.border-radius(6px);}}border: 1px solid darken(@wellBackground, 70%);}.well-lg{padding: 24px;border-radius: @border-radius-large;}@media (min-width: @screen-sm){margin: 10px;.box-sizing(border-box);}.well-large{padding: 24px;.border-radius(6px);}.radio input[type=\"radio\"],.radio-inline input[type=\"radio\"],.checkbox input[type=\"checkbox\"],.checkbox-inline input[type=\"checkbox\"]{float: left;margin-left: -20px;}";
 		FileUtil f = new FileUtil();
 		URL src_path = FileUtilTest.class.getResource("/less/wells.orig.less");
 		LessParser orig = new LessParser(f.readFile(new File(src_path.getPath())));
@@ -43,6 +44,7 @@ public class FileUtilTest {
 
 		LessMerger lm  = new LessMerger(orig, over);
 		lm.merge();
-		System.out.println(orig.toString());
+		
+		Assert.assertEquals(expected,orig.toString());
 	}
 }
