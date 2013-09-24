1a190359d1ccb8da26a115a6de59678cbc77b9f2
Kevin Mulvey <king.feruke@gmail.com>
2013-09-23 20:46:58 -0400

    block tests

--- src/test/java/BlockTest.java
+++ src/test/java/BlockTest.java
@@ -9,21 +9,39 @@ public class BlockTest {
 	@Test
 	public void setSelectorTest() {
 		Block b = new Block(".form-emphasis");
+		Assert.assertEquals(null, b.getAction());
 		Assert.assertEquals(".form-emphasis", b.getSelector());
 	}
 	@Test
 	public void setCompoundSelectorTest() {
 		Block b = new Block("  form.form-emphasis input[type=radio]  ");
+		Assert.assertEquals(null, b.getAction());
 		Assert.assertEquals("form.form-emphasis input[type=radio]", b.getSelector());
 	}
 	@Test
 	public void deleteBlockTest() {
 		Block b = new Block("-.form-emphasis");
 		Assert.assertEquals("remove", b.getAction());
+		Assert.assertEquals(".form-emphasis", b.getSelector());
 	}
 	@Test
 	public void addBlockTest() {
 		Block b = new Block("  +  .form-emphasis");
 		Assert.assertEquals("add", b.getAction());
+		Assert.assertEquals(".form-emphasis", b.getSelector());
+
 	}
+	@Test
+	public void quotedBlockTest() {
+		Block b = new Block("input[type=\"radio\"],  input[type=\"checkbox\"]");
+		Assert.assertEquals(null, b.getAction());
+		Assert.assertEquals("input[type=\"radio\"],  input[type=\"checkbox\"]", b.getSelector());
+	}
+	@Test
+	public void mediaQueryBlockTest() {
+		Block b = new Block("@media (min-width: @screen-sm)");
+		Assert.assertEquals(null, b.getAction());
+		Assert.assertEquals("@media (min-width: @screen-sm)", b.getSelector());
+	}
+	
 }
