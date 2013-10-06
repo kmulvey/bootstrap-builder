972683c18121fd663de1899d8b7c4f973b9779f0
Kevin Mulvey <kmulvey@shutterstock.com>
2013-10-06 10:29:09 -0400

    handle IE hacks

--- src/main/java/Property.java
+++ src/main/java/Property.java
@@ -9,35 +9,54 @@ public class Property extends LessObject {
 
 	public Property(String prop) {
 		logger.entry();
-		if(prop.startsWith("-")) {
+		if (prop.startsWith("-")) {
 			action = "remove";
 			prop = prop.substring(1).trim();
-		}
-		else if(prop.startsWith("+")) {
+		} else if (prop.startsWith("+")) {
 			action = "add";
 			prop = prop.substring(1).trim();
 		}
-		
-		
+
 		if (prop.trim().contains(":")) {
 			String[] property = prop.split(":");
-			name = property[0].trim();
-			value = property[1].trim();
-		} 
+
+			// IE hacks like progid:DXImageTransform screw this up
+			if (property.length > 2) {
+				String buffer = "";
+				boolean first_colon = false;
+				for (int i = 0; i < prop.length(); i++) {
+					char c = prop.charAt(i);
+					if (c == ':' && first_colon == false) {
+						name = buffer;
+						first_colon = true;
+						buffer = "";
+					} else {
+						buffer += c;
+					}
+				}
+				value = buffer;
+			} else {
+				name = property[0].trim();
+				value = property[1].trim();
+			}
+		}
 		// this looks like a mixin
-		else{
+		else {
 			name = prop.trim();
 			value = "";
 			mixin = true;
 		}
 	}
-	public String getName(){
+
+	public String getName() {
 		return logger.exit(name);
 	}
-	public String getValue(){
+
+	public String getValue() {
 		return logger.exit(value);
 	}
-	public Boolean isMixin(){
+
+	public Boolean isMixin() {
 		return logger.exit(mixin);
 	}
 }
