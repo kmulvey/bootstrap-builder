a0577754cb5bb692c7b93c725f2d6d329b502bbd
Kevin Mulvey <kmulvey@shutterstock.com>
2013-11-30 10:27:51 -0500

    some tests for source files and override

--- src/test/java/LessParserTests.java
+++ src/test/java/LessParserTests.java
@@ -49,4 +49,40 @@ public class LessParserTests {
 		Assert.assertEquals(7, prop);
 		Assert.assertEquals(1, block);
 	}
+	@Test
+	public void simpleOverrideFileTest() {
+		FileUtil f = new FileUtil();
+		URL file_path = FileUtilTest.class.getResource("/less/wells.less");
+		File test_file = new File(file_path.getPath());
+		String file_contents = f.readFile(test_file);
+	
+		LessParser orig = new LessParser(file_contents);
+		orig.parseLess();
+		
+		// how many blocks are in the file?
+		Assert.assertEquals(6, orig.getLessFile().size());
+
+		// look more deeply into the first block
+		Assert.assertEquals(Block.class, orig.getLessFile().get(0).getClass());
+
+		// count number of children in first block
+		Block first_ele = (Block) orig.getLessFile().get(0);
+		Assert.assertEquals(5, first_ele.children.size());
+		
+		// how many of each type?
+		int prop = 0, block = 0;
+		for(int i = 0; i<first_ele.children.size(); i++){
+			if(first_ele.children.get(i) instanceof Block){
+				block++;
+			}
+			else if(first_ele.children.get(i) instanceof Property){
+				prop++;
+			}
+			else{
+				Assert.fail("the nuber of props and blocks is wrong");
+			}
+		}
+		Assert.assertEquals(4, prop);
+		Assert.assertEquals(1, block);
+	}
 }
