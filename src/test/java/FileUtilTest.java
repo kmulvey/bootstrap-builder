15ffce5c7fe33e0c081470c7854aeaef8353e50f
Kevin Mulvey <king.feruke@gmail.com>
2014-01-28 09:32:20 -0500

    tests are how we run

--- src/test/java/FileUtilTest.java
+++ src/test/java/FileUtilTest.java
@@ -7,28 +7,43 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import com.ss.less.LessMerger;
+import com.ss.less.LessMergerII;
+import com.ss.less.LessParser;
 import com.ss.less.utils.FileUtil;
 
 @RunWith(JUnit4.class)
 public class FileUtilTest {
 
-	@Test
-	public void findFiles() {
-		FileUtil f = new FileUtil();
-		ArrayList<File> etc_files = f.findFiles("/etc");
-		Assert.assertEquals(false, etc_files.isEmpty());
-	}
+//	@Test
+//	public void findFiles() {
+//		FileUtil f = new FileUtil();
+//		ArrayList<File> etc_files = f.findFiles("/etc");
+//		Assert.assertEquals(false, etc_files.isEmpty());
+//	}
+//
+//	@Test
+//	public void readFileTest() {
+//		String expected = "this is a test file and its not empty.";
+//		FileUtil f = new FileUtil();
+//		URL file_path = FileUtilTest.class.getResource("/testfile.txt");
+//		File test_file = new File(file_path.getPath());
+//		String file_contents = f.readFile(test_file);
+//		file_contents = file_contents.replaceAll("[ \t]+$", "");
+//		Assert.assertEquals(expected, file_contents);
+//		Assert.assertEquals(false, file_contents.contains("block"));
+//		Assert.assertEquals(false, file_contents.contains("single"));
+//	}
 
 	@Test
-	public void readFileTest() {
-		String expected = "this is a test file and its not empty.";
+	public void mergetwo() {
 		FileUtil f = new FileUtil();
-		URL file_path = FileUtilTest.class.getResource("/testfile.txt");
-		File test_file = new File(file_path.getPath());
-		String file_contents = f.readFile(test_file);
-		file_contents = file_contents.replaceAll("[ \t]+$", "");
-		Assert.assertEquals(expected, file_contents);
-		Assert.assertEquals(false, file_contents.contains("block"));
-		Assert.assertEquals(false, file_contents.contains("single"));
+		LessParser orig = new LessParser(f.readFile(new File("/var/www/bootstrap-kmulvey/less/alerts.less")));
+		LessParser over = new LessParser(f.readFile(new File("/var/www/bootstrap-kmulvey/less/shutterstock/alerts.less")));
+
+		over.parseLess(false);
+		orig.parseLess(true);
+
+		LessMergerII lm = new LessMergerII(orig, over);
 	}
 }
