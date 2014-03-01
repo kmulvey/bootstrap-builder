703fd67bc61d6a58cb70688a017bebb8f188d80a
Kevin Mulvey <king.feruke@gmail.com>
2014-03-01 09:23:36 -0500

    formatting

--- src/main/java/com/ss/less/runners/LessRunner.java
+++ src/main/java/com/ss/less/runners/LessRunner.java
@@ -1,6 +1,5 @@
 package com.ss.less.runners;
 
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -8,23 +7,22 @@ import com.ss.less.LessMerger;
 import com.ss.less.LessParser;
 import com.ss.less.utils.FileUtil;
 
-
 public class LessRunner implements Runnable {
-  private String override, original, file_name, output_dir;
-  private Logger logger = LogManager.getLogger(LessRunner.class.getName());
+	private String override, original, file_name, output_dir;
+	private Logger logger = LogManager.getLogger(LessRunner.class.getName());
 
-  LessRunner(String orig, String over, String f_name, String o_dir) {
-    this.override = over;
-    this.original = orig;
-    this.output_dir = o_dir;
-    this.file_name = f_name;
-  }
+	LessRunner(String orig, String over, String f_name, String o_dir) {
+		this.override = over;
+		this.original = orig;
+		this.output_dir = o_dir;
+		this.file_name = f_name;
+	}
 
-  @Override
-  public void run() {
+	@Override
+	public void run() {
 		LessParser orig = new LessParser(original);
 		LessParser over = new LessParser(override);
-		
+
 		over.parseLess(false);
 		orig.parseLess(true);
 
@@ -34,5 +32,5 @@ public class LessRunner implements Runnable {
 		FileUtil f = new FileUtil();
 		f.writeFile(output_dir, file_name, orig.toString());
 		logger.info("merged file " + file_name);
-  }
-} 
+	}
+}
