a247a83e2bfed951b514d0a7a8a238dfca6dca8a
Kevin Mulvey <king.feruke@gmail.com>
2014-03-05 19:57:53 -0500

    remove mixin rename and formatting

--- src/test/java/BlockTest.java
+++ src/test/java/BlockTest.java
@@ -6,7 +6,6 @@ import org.junit.runners.JUnit4;
 import com.ss.less.objects.Block;
 
 @RunWith(JUnit4.class)
-
 public class BlockTest {
 	@Test
 	public void setSelectorTest() {
@@ -15,6 +14,7 @@ public class BlockTest {
 		Assert.assertEquals(null, b.getAction());
 		Assert.assertEquals(".form-emphasis", b.getSelector());
 	}
+
 	@Test
 	public void setCompoundSelectorTest() {
 		Block b = new Block("  form.form-emphasis input[type=radio]  ");
@@ -22,6 +22,7 @@ public class BlockTest {
 		Assert.assertEquals(null, b.getAction());
 		Assert.assertEquals("form.form-emphasis input[type=radio]", b.getSelector());
 	}
+
 	@Test
 	public void deleteBlockTest() {
 		Block b = new Block("-.form-emphasis");
@@ -29,6 +30,7 @@ public class BlockTest {
 		Assert.assertEquals("remove", b.getAction());
 		Assert.assertEquals(".form-emphasis", b.getSelector());
 	}
+
 	// test a + sign in a source file
 	@Test
 	public void plusInSrcTest() {
@@ -38,6 +40,7 @@ public class BlockTest {
 		Assert.assertEquals(null, b.getAction());
 		Assert.assertEquals("+  .form-emphasis", b.getSelector());
 	}
+
 	// test adding a block with + sign
 	@Test
 	public void addBlockTest() {
@@ -46,6 +49,7 @@ public class BlockTest {
 		Assert.assertEquals("add", b.getAction());
 		Assert.assertEquals(".form-emphasis", b.getSelector());
 	}
+
 	@Test
 	public void replaceBlockTest() {
 		Block b = new Block(".form-emphasis%.form-basic");
@@ -54,6 +58,7 @@ public class BlockTest {
 		Assert.assertEquals(".form-emphasis", b.getSelector());
 		Assert.assertEquals(2, b.getupdatedSelector().length);
 	}
+
 	@Test
 	public void replaceBlockSpacesTest() {
 		Block b = new Block(".form-emphasis   %    .form-basic    ");
@@ -62,6 +67,7 @@ public class BlockTest {
 		Assert.assertEquals(".form-emphasis", b.getSelector());
 		Assert.assertEquals(2, b.getupdatedSelector().length);
 	}
+
 	@Test
 	public void quotedBlockTest() {
 		Block b = new Block("input[type=\"radio\"],  input[type=\"checkbox\"]");
@@ -69,6 +75,7 @@ public class BlockTest {
 		Assert.assertEquals(null, b.getAction());
 		Assert.assertEquals("input[type=\"radio\"],input[type=\"checkbox\"]", b.getSelector());
 	}
+
 	@Test
 	public void mediaQueryBlockTest() {
 		Block b = new Block("@media (min-width: @screen-sm)");
@@ -76,6 +83,7 @@ public class BlockTest {
 		Assert.assertEquals(null, b.getAction());
 		Assert.assertEquals("@media (min-width: @screen-sm)", b.getSelector());
 	}
+
 	@Test
 	public void mediaQueryAddBlockTest() {
 		Block b = new Block("+  @media (min-width: @screen-sm)");
@@ -83,6 +91,7 @@ public class BlockTest {
 		Assert.assertEquals("add", b.getAction());
 		Assert.assertEquals("@media (min-width: @screen-sm)", b.getSelector());
 	}
+
 	@Test
 	public void mediaQueryRemoveBlockTest() {
 		Block b = new Block("-     @media (min-width: @screen-sm)");
@@ -90,6 +99,7 @@ public class BlockTest {
 		Assert.assertEquals("remove", b.getAction());
 		Assert.assertEquals("@media (min-width: @screen-sm)", b.getSelector());
 	}
+
 	@Test
 	public void removeCompoundBlockTest() {
 		Block b = new Block("-     (~\"input.span@{index}, textarea.span@{index}, .uneditable-input.span@{index}\")");
@@ -97,6 +107,7 @@ public class BlockTest {
 		Assert.assertEquals("remove", b.getAction());
 		Assert.assertEquals("(~\"input.span@{index},textarea.span@{index},.uneditable-input.span@{index}\")", b.getSelector());
 	}
+
 	@Test
 	public void percentValBlockTest() {
 		Block b = new Block(".vertical-three-colors(@startColor: #00b3ee, @midColor: #7a43b6, @colorStop: 50%, @endColor: #c3325f)");
@@ -104,14 +115,7 @@ public class BlockTest {
 		Assert.assertEquals(null, b.getAction());
 		Assert.assertEquals(".vertical-three-colors(@startColor: #00b3ee,@midColor: #7a43b6,@colorStop: 50%,@endColor: #c3325f)", b.getSelector());
 	}
-//	@Test
-//	public void renameMixin() {
-//		Block b = new Block(".box-shadow(@shadow)%.box-shadow(@shadowA, @shadowB:X, ...)");
-//		b.process();
-//		Assert.assertEquals("update", b.getAction());
-//		Assert.assertEquals(".box-shadow(@shadow)", b.getSelector());
-//		Assert.assertEquals(".box-shadow(@shadowA,@shadowB:X,...)", b.getupdatedSelector()[1]);
-//	}
+
 	@Test
 	public void setSelector() {
 		Block b = new Block("  +  .form-emphasis");
